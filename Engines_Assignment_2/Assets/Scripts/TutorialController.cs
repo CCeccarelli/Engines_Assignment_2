f25623ebd146685c142ad71b575dbe641c126df8
Unknown <hugo.suzuki@uoit.net>
2019-11-05 09:49:13 -0500

    Health Tutorial Done
    tutorial script commented

--- Engines_Assignment_2/Assets/Scripts/TutorialController.cs
+++ Engines_Assignment_2/Assets/Scripts/TutorialController.cs
@@ -8,6 +8,7 @@ public class TutorialController : MonoBehaviour
     public GameObject TutorialObj;
     public EnemyControl e;
     public Text TutorialText;
+    public Text TutorialHealth;
     public bool tutorial;
     bool healthTutorial;
     int timer;
@@ -33,27 +34,37 @@ public class TutorialController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (ListOfPos.Count == 0)
+        if (ListOfPos.Count == 0) // If Tutorial Block moved to all 3 positions
         {
-            TutorialObj.SetActive(false);
-            TutorialText.text = "Hit as many enemies as possible to get a high score!";
-            if (timer == 250 && healthTutorial == false)
+            TutorialObj.SetActive(false); // Deactivate Tutorial Object
+
+            if (healthTutorial == false) // If Health Tutorial wasn't shown
             {
                 TutorialText.text = "If you miss a target you'll lose health. If it reaches 0 you Lose!";
-                timer = 0;
-                healthTutorial = true;
+                TutorialHealth.color = new Vector4(1, 0, 0, 1); 
+                if (timer == 100) 
+                {
+                    timer = 0;
+                    healthTutorial = true;
+                }
             }
-            if (timer == 300)
+            else // If Health Tutorial has been done
             {
-                tutorial = false;
-                e.GetComponent<EnemyControl>().initializeEnemies();
-                ListOfPos.Add(new Vector3(0f, 0f, 0f));
-                ListOfPos.Add(new Vector3(0f, 0f, 0f));
-                TutorialText.color = new Vector4(0f, 0f, 0f, 0f);
+                TutorialText.text = "Hit as many enemies as possible to get a high score!";
+                if (timer == 180)
+                {
+                    tutorial = false;
+                    e.GetComponent<EnemyControl>().initializeEnemies();
+                    ListOfPos.Add(new Vector3(0f, 0f, 0f));
+                    ListOfPos.Add(new Vector3(0f, 0f, 0f));
+                    TutorialText.color = new Vector4(0, 0, 0, 0);
+                    TutorialHealth.color = new Vector4(0, 0, 0, 0);
+                }
             }
             timer++;
         }
-        else if (ListOfPos.Count == 1)
+
+        else if (ListOfPos.Count == 1) // If player is on the last object position, wait for click and remove object instead of lerping it.
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
@@ -66,14 +77,17 @@ public class TutorialController : MonoBehaviour
                 }
             }
         }
-        else
+
+        else // If block still has more positions to travel to
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (timer == 100)
+            if (timer == 100) // Wait to change message and activate tutorial enemy
             {
                 TutorialText.text = "Aim at the red square <enemy> and use the left mouse button to hit it";
                 TutorialObj.SetActive(true);
+                timer = 0;
             }
+
             timer++;
 
             if (move == true)
@@ -81,6 +95,7 @@ public class TutorialController : MonoBehaviour
                 TutorialObj.transform.position = Vector3.Lerp(PrevPos, ListOfPos[0], count);
                 count += 0.01f;
             }
+
             if (TutorialObj.transform.position == ListOfPos[0])
             {
                 PrevPos = TutorialObj.transform.position;
@@ -93,20 +108,38 @@ public class TutorialController : MonoBehaviour
     }
 
 
-    public void tutorialRay()
+    public void tutorialRay() // Raycast to identify object Hit
     {
         if (Physics.Raycast(ray, out hit))
         {
             Transform objectHit = hit.transform;
-            if (Input.GetMouseButtonDown(0) && hit.transform.tag == "tutorialObj")
+            if (Input.GetMouseButtonDown(0) && hit.transform.tag == "tutorialObj") // If Player hits the object
             {
                 move = true;
             }
-            else if (Input.GetMouseButtonDown(0) && hit.transform.tag == "tutorialObj")
+            else if (Input.GetMouseButtonDown(0)) // If Player Misses the object and Hits the "Floor"
             {
-                
-                TutorialText.text = "If you miss a target you'll lose health. If it reaches 0 you Lose!";
-                healthTutorial = true;
+                if (healthTutorial == false)
+                {
+                    TutorialHealth.text = "Health: <color=red>2</color>";
+                    TutorialText.text = "If you miss a target you'll lose health. If it reaches 0 you Lose!";
+                    healthTutorial = true;
+                    timer = -50;
+                }
+            }
+        }
+
+        else // If Raycast doesn't hit a Game Object
+        {
+            if (Input.GetMouseButtonDown(0)) // And Mouse Button is pressed aiming at nothing
+            {
+                if (healthTutorial == false)
+                {
+                    TutorialHealth.text = "Health: <color=red>2</color>";
+                    TutorialText.text = "If you miss a target you'll lose health. If it reaches 0 you Lose!";
+                    healthTutorial = true;
+                    timer = -50;
+                }
             }
         }
     }
